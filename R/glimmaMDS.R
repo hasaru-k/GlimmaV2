b4dd07d0ce23e36c8b216f5d12468116c6679e74
hasacat <hasaru.kariyawasam@gmail.com>
2020-07-09 17:01:55 +1000

    - split MDS, XY/MA files

---
+++ R/glimmaMDS.R
@@ -0,0 +1,272 @@
+#' Glimma MDS Plot
+#'
+#' Draws a two-panel interactive MDS plot in an html page. The left panel contains the plot between
+#' two MDS dimensions, with annotations displayed on hover. The right panel contains a bar plot of
+#' the eigenvalues of each dimension. The controls below can be used to change the dimensions being
+#' displayed.
+#'
+#' @seealso \code{\link{glimmaMDS.default}}, \code{\link{glimmaMDS.DGEList}}, \code{\link{glimmaMDS.DESeqDataSet}}
+#'
+#' @param x the matrix containing the gene expressions.
+#' @param ... the additional arguments.
+#' @export
+glimmaMDS <- function(x, ...)
+{
+  UseMethod("glimmaMDS")
+}
+
+#' Glimma MDS Plot
+#'
+#' Draws a two-panel interactive MDS plot. The left panel contains the plot between
+#' two MDS dimensions, with annotations displayed on hover. The right panel contains a bar plot of
+#' the eigenvalues of each dimension. The controls below can be used to change the dimensions being
+#' displayed.
+#'
+#' @seealso \code{\link{glimmaMDS.DGEList}}, \code{\link{glimmaMDS.DESeqDataSet}}
+#'
+#' @param x the matrix containing the gene expressions.
+#' @param top the number of top most variable genes to use.
+#' @param labels the labels for each sample.
+#' @param groups the experimental group to which samples belong.
+#' @param gene.selection 	"pairwise" if most variable genes are to be chosen for each pair of samples or
+#' "common" to select the same genes for all comparisons.
+#' @param continuous.colour if TRUE, colour is displayed using continuous columns in groups; if false,
+#' colour is displayed using discrete columns.
+#' @param width custom widget width in pixels
+#' @param height custom widget height in pixels
+#' @export
+glimmaMDS.default <- function(
+  x,
+  top = 500,
+  labels = as.character(seq_len(ncol(x))),
+  groups = as.character(rep(1, ncol(x))),
+  gene.selection = c("pairwise", "common"),
+  continuous.colour=FALSE,
+  width = 900, 
+  height = 570)
+{
+
+  # helper function
+  getCols <- function(x, inds) {
+      x[, inds, drop=FALSE]
+  }
+
+  # CODE TAKEN FROM GLIMMA
+
+  #   Multi-dimensional scaling with top-distance
+  #   Di Wu and Gordon Smyth
+  #   19 March 2009.  Last modified 14 Jan 2015
+  #   Modified by Shian Su on 25 Jan 2016
+
+  ##
+  # Check Inputs
+
+  x <- as.matrix(x)
+  nsamples <- ncol(x)
+  ndim <- nsamples - 1
+
+  if (nsamples < 3) {
+      stop(paste("Only", nsamples, "columns of data: need at least 3"))
+  }
+
+  cn <- colnames(x)
+  bad <- rowSums(is.finite(x)) < nsamples
+
+  if (any(bad)) {
+      warning("Rows containing infinite values have been removed")
+      x <- x[!bad, , drop=FALSE]
+  }
+
+  nprobes <- nrow(x)
+  top <- min(top, nprobes)
+
+  #
+  ##
+
+  gene.selection <- match.arg(gene.selection, c("pairwise", "common"))
+
+  # Distance matrix from pairwise leading fold changes
+  dd <- matrix(0, nrow=nsamples, ncol=nsamples, dimnames=list(cn, cn))
+  if (gene.selection == "pairwise") {
+  # Distance measure is mean of top squared deviations for each pair of arrays
+      topindex <- nprobes - top + 1L
+      for (i in 2L:(nsamples)) {
+          for (j in 1L:(i - 1L)) {
+              dists <- (getCols(x, i) - getCols(x, j))^2
+              dists <- sort.int(dists, partial = topindex )
+              topdist <- dists[topindex:nprobes]
+              dd[i, j] <- sqrt(mean(topdist))
+          }
+      }
+  } else 
+  {
+  # Same genes used for all comparisons
+      if (nprobes > top) {
+          o <- order(rowMeans( (x-rowMeans(x))^2 ), decreasing=TRUE)
+          x <- getRows(x, o[1L:top])
+      }
+      for (i in 2L:(nsamples)) {
+          dists <- (x[, i] - x[, 1:(i-1), drop=FALSE]) ^ 2
+          dd[i, 1L:(i-1L)] <- sqrt(colMeans(dists))
+      }
+  }
+
+  # Multi-dimensional scaling
+  a1 <- suppressWarnings(cmdscale(as.dist(dd), k=min(ndim, 8), eig=TRUE))
+
+  # Method for MDS objects
+  points <- a1$points
+
+  if (!is.data.frame(groups) && class(groups) != "DataFrame") groups <- data.frame(groups)
+
+
+
+  # add labels to groups
+  groups <- data.frame(labels, groups)
+  # ensure there are no periods in groups colnames (Vega doesn't handle these well)
+  colnames(groups) <- gsub("\\.", "_", colnames(groups))
+
+  points <- data.frame(points)
+  dimlist <- paste0("dim", seq_len(ncol(points)))
+  names(points) <- paste0("dim", seq_len(ncol(points)))
+  points <- data.frame(points, groups)
+
+  eigen <- data.frame(
+      name = 1:min(ndim, 8),
+      eigen = round(a1$eig[1:min(ndim, 8)]/sum(a1$eig), 2)
+  )
+
+  # add this column for no dimensionality in Vega
+  points <- cbind(points, "-" = "0")
+  points <- cbind(points, "- " = 0)
+  is_numeric <- sapply(groups, is.numeric)
+  numeric <- c(colnames(groups[, is_numeric]), "- ")
+  discrete <- c(colnames(groups[, !is_numeric]), "-")
+  features <- list(numeric=numeric, discrete=discrete, all=c(numeric,discrete))
+
+  # forward data to the widget using xData
+  xData = list(plotType = "MDS",
+               data = list(mdsData=points,
+                           eigenData=eigen,
+                           features=features,
+                           continuous_colour=continuous.colour,
+                           dimlist=dimlist))
+
+
+  # create widget
+  return(htmlwidgets::createWidget(
+    name = 'GlimmaV2',
+    xData,
+    width = width,
+    height = height,
+    package = 'GlimmaV2',
+    elementId = NULL,
+    sizingPolicy = htmlwidgets::sizingPolicy(defaultWidth=750, defaultHeight=750, browser.fill=TRUE, viewer.suppress=TRUE)
+  ))
+
+}
+
+#' Glimma MDS Plot
+#'
+#' Draws a two-panel interactive MDS plot using a DGEList x.
+#' By default, extracts \code{labels} as \code{rownames(x$samples)}.
+#' @seealso \code{\link{glimmaMDS.default}}, \code{\link{glimmaMDS.DESeqDataSet}}
+#'
+#' @inheritParams glimmaMDS.default
+#' @param prior.count average count to be added to each observation to avoid taking log of zero.
+#' @export
+glimmaMDS.DGEList <- function(
+  x,
+  top = 500,
+  labels = NULL,
+  groups = as.character(rep(1, ncol(x))),
+  gene.selection = c("pairwise", "common"),
+  prior.count = 2,
+  continuous.colour = FALSE,
+  width = 900, 
+  height = 570)
+{
+
+  # extract sample groups based on DGEList class, if we need to
+  if (is.null(labels))
+  {
+    if (!is.null(x$samples$groups))
+    {
+      labels <- rownames(x$samples)
+    } else
+    {
+      labels <- as.character(seq_len(ncol(x)))
+    }
+  }
+
+  transformed_counts <- edgeR::cpm(x, log=TRUE, prior.count = prior.count)
+
+  # call main processing function
+  return(glimmaMDS.default(
+    transformed_counts,
+    top=top,
+    labels=labels,
+    groups=groups,
+    gene.selection=gene.selection,
+    continuous.colour=continuous.colour))
+
+}
+
+#' Glimma MDS Plot
+#'
+#' Draws a two-panel interactive MDS plot using a DESeqDataset x. 
+#' By default, extracts groups as \code{colData(x)} and extracts labels as \code{rownames(colData(x))}.
+#' @seealso \code{\link{glimmaMDS.default}}, \code{\link{glimmaMDS.DGEList}}
+#'
+#' @inheritParams glimmaMDS.DGEList
+#' @export
+glimmaMDS.DESeqDataSet <- function(
+  x,
+  top = 500,
+  labels = NULL,
+  groups = NULL,
+  gene.selection = c("pairwise", "common"),
+  prior.count = 0.25,
+  continuous.colour = FALSE,
+  width = 900, 
+  height = 570)
+{
+
+  # extract sample groups based on DESeqDataSet class, if we need to
+  if (is.null(labels)) 
+  {
+    if (!is.null(SummarizedExperiment::colData(x))) 
+    {
+      labels <- rownames(SummarizedExperiment::colData(x))
+    } else 
+    {
+      labels <- as.character(seq_len(ncol(x)))
+    }
+  }
+
+  transformed_counts <- edgeR::cpm(
+      DESeq2::counts(x),
+      log = TRUE,
+      prior.count = prior.count
+  )
+
+  if (is.null(groups)) 
+  {
+    if (!is.null(SummarizedExperiment::colData(x))) 
+    {
+        groups <- S4Vectors::as.data.frame.DataTable(SummarizedExperiment::colData(x))
+    } else 
+    {
+        groups <- as.character(rep(1, ncol(x)))
+    }
+  }
+
+  return(glimmaMDS.default(
+    transformed_counts,
+    top=top,
+    labels=labels,
+    groups=groups,
+    gene.selection=gene.selection,
+    continuous.colour=continuous.colour))
+
+}
---
+++ R/glimmaXY.R
@@ -0,0 +1,249 @@
+#' Glimma MA Plot
+#'
+#' @export
+glimmaMA <- function(x, ...)
+{
+  UseMethod("glimmaMA")
+}
+
+#' Glimma MA Plot
+#'
+#' @export
+glimmaMA.default <- function(
+  x,
+  status=rep(0, nrow(x)),
+  coef=ncol(x$coefficients),
+  main=colnames(x)[coef],
+  p.adj.method = "BH",
+  display.columns = NULL,
+  anno=NULL,
+  counts=NULL,
+  xlab=NULL,
+  ylab=NULL,
+  status.colours=c("dodgerblue", "lightslategray", "firebrick"),
+  width = 900, 
+  height = 570)
+{
+  # create initial table with logCPM and logFC features
+  xvals <- unname(x$Amean)
+  yvals <- unname(x$coefficients[, coef])
+  stopifnot(all(names(x$Amean) == names(x$coefficients[, coef])))
+  if (is.null(xlab)) xlab <- "logCPM"
+  if (is.null(ylab)) ylab <- "logFC"
+  table <- data.frame(xvals, yvals)
+  names(table) <- c(xlab, ylab)
+
+  # add pvalue/adjusted pvalue info from fit object to table
+  AdjPValue <- stats::p.adjust(x$p.value[, coef], method=p.adj.method)
+  table <- cbind(table, PValue=x$p.value[, coef], AdjPValue=AdjPValue)
+
+  # add gene info from MArrayLM object to table
+  table <- cbind(x$genes, table)
+
+  # make status single-dimensional
+  if (is.matrix(status)) status <- status[, coef]
+  if (length(status)!=nrow(table)) stop("Status vector must have the same number of genes as x arg.")
+  xData <- buildXYData(table, status, main, display.columns, anno, counts, xlab, ylab, status.colours)
+  return(glimmaXYWidget(xData, width, height))
+}
+
+#' Glimma MA Plot
+#'
+#' @export
+glimmaMA.DESeqDataSet  <- function(
+  x,
+  status=NULL,
+  main="MA Plot",
+  display.columns = NULL,
+  anno=NULL,
+  counts=NULL,
+  xlab=NULL,
+  ylab=NULL,
+  status.colours=c("dodgerblue", "lightslategray", "firebrick"),
+  width = 900, 
+  height = 570)
+{
+
+  # extract logCPM, logFC from DESeqDataSet
+  res <- DESeq2::results(x)
+  res.df <- as.data.frame(res)
+  delRows <- naRowInds(res.df, "log2FoldChange", "padj")
+  res.df <- res.df[!delRows, , drop=FALSE]
+  anno <- anno[!delRows, , drop=FALSE]
+
+  # extract status if it is not given
+  if (is.null(status)) status <- as.numeric(res$padj<0.05)
+  status <- status[!delRows]
+
+  # create initial table with logCPM and logFC features
+  xvals <- log(res.df$baseMean + 0.5)
+  yvals <- res.df$log2FoldChange
+  if (is.null(xlab)) xlab <- "logCPM"
+  if (is.null(ylab)) ylab <- "logFC"
+  table <- data.frame(xvals, yvals)
+  names(table) <- c(xlab, ylab)
+
+  # add pvalue/adjusted pvalue info from fit object to table
+  table <- cbind(table, PValue=res.df$pvalue, AdjPValue=res.df$padj)
+
+  # make status single-dimensional
+  if (is.matrix(status)) status <- status[, coef]
+  if (length(status)!=nrow(table)) stop("Status vector must have the same number of genes as x arg.")
+  xData <- buildXYData(table, status, main, display.columns, anno, counts, xlab, ylab, status.colours)
+  return(glimmaXYWidget(xData, width, height))
+}
+
+# returns indices of NA rows
+naRowInds <- function(res.df, ...) 
+{
+  res.df <- data.frame(res.df)
+  filterCols <- unlist(list(...))
+
+  delRows <- rep(FALSE, nrow(res.df))
+
+  for (cols in filterCols) 
+  {
+      delRows <- delRows | is.na(res.df[, cols])
+  }
+  return(delRows)
+}
+
+#' Glimma XY Plot
+#'
+#' @export
+glimmaXY <- function(x, ...)
+{
+  UseMethod("glimmaXY")
+}
+
+#' Glimma XY Plot
+#'
+#' @export
+glimmaXY.default <- function(
+  x,
+  y, 
+  xlab="x",
+  ylab="y", 
+  status=rep(0, length(x)),
+  main="XY Plot",
+  display.columns = NULL,
+  anno=NULL,
+  counts=NULL,
+  status.colours=c("dodgerblue", "lightslategray", "firebrick"),
+  width = 900, 
+  height = 570)
+{
+  if (length(x)!=length(y)) stop("Error: x and y args must have the same length.")
+  table <- data.frame(x, y) 
+  names(table) <- c(xlab, ylab)
+  if (length(status)!=nrow(table)) stop("Status vector must have the same number of genes as x/y args.")
+  xData <- buildXYData(table, status, main, display.columns, anno, counts, xlab, ylab, status.colours)
+  return(glimmaXYWidget(xData, width, height))
+}
+
+# common processing for both MA and XY plots.
+# expects a table with xlab and ylab columns
+# and a 1D status vector.
+buildXYData <- function(
+  table,
+  status,
+  main,
+  display.columns = NULL,
+  anno=NULL,
+  counts=NULL,
+  xlab=NULL,
+  ylab=NULL,
+  status.colours=c("dodgerblue", "lightslategray", "firebrick"))
+{
+
+  # give placeholder for counts
+  if (is.null(counts)) counts <- -1
+  
+  # add colour and anno info to table
+  table <- cbind(table, status=as.vector(status))
+  if (!is.null(anno)) table <- cbind(table, anno)
+
+  # add index for linking table and plot (independent of object type) to table
+  table <- data.frame(index=1:nrow(table), table)
+
+  # set display.columns (columns to show in tooltips and in the table)
+  if (is.null(display.columns)) 
+  {
+    display.columns <- colnames(table)
+  } else
+  {
+    # if it's specified, make sure at least x, y and index are shown
+    if (!(xlab %in% display.columns)) display.columns <- c(display.columns, xlab)
+    if (!(ylab %in% display.columns)) display.columns <- c(display.columns, ylab)
+    if (!("index" %in% display.columns)) display.columns <- c("index", display.columns)
+  }
+
+  # error checking on status_colours
+  if (length(status.colours) != 3) stop("status_colours 
+          arg must have exactly 3 elements for [downreg, notDE, upreg]")
+  
+  xData <- list(plotType="XY",
+                data=list(x=xlab, 
+                          y=ylab, 
+                          table=table, 
+                          cols=display.columns,
+                          counts=counts, 
+                          tooltipFields=display.columns,
+                          status_colours=status.colours,
+                          title=main))
+  return(xData)
+}
+
+#' GlimmaXY HTMLWidget Wrapper
+#'
+#' Passes packaged data to JS interface for rendering.
+#'
+#' @import htmlwidgets
+glimmaXYWidget <- function(
+  xData,
+  width = 900,
+  height = 570,
+  elementId = NULL,
+  ...)
+{
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'GlimmaV2',
+    xData,
+    width = width,
+    height = height,
+    package = 'GlimmaV2',
+    elementId = elementId,
+    sizingPolicy = htmlwidgets::sizingPolicy(defaultWidth=750, defaultHeight=750, browser.fill=TRUE, viewer.suppress=TRUE)
+  )
+
+}
+
+#' Shiny bindings for GlimmaV2
+#'
+#' Output and render functions for using GlimmaV2 within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a GlimmaV2
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name GlimmaV2-shiny
+#'
+#' @export
+GlimmaV2Output <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'GlimmaV2', width, height, package = 'GlimmaV2')
+}
+
+#' @rdname GlimmaV2-shiny
+#' @export
+renderGlimmaV2 <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, GlimmaV2Output, env, quoted = TRUE)
+}
