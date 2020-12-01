14450b673f827d3d44fcd08e7853e07e4f3a0449
hasaru-k <hasaru-k@users.noreply.github.com>
2020-12-01 12:03:36 +1100

    represent state as class

--- inst/htmlwidgets/glimmaXY.js
+++ inst/htmlwidgets/glimmaXY.js
@@ -88,57 +88,87 @@ HTMLWidgets.widget({
   }
 });
 
-function getStateMachine(data)
-{
-  var state = 
-  { 
-    selected: [], 
-    graphMode: false,
-    setSelected: function(selected) {
-      this.selected = selected;
-      this.selectedUpdateHandler();
-    },
-    toggleGene: function(gene) {
-      let loc = containsGene(this.selected, gene);
-      var new_arr = loc >= 0 ? remove(this.selected, loc) : this.selected.concat(gene);
-      this.setSelected(new_arr);
-      this.expressionUpdateHandler(loc < 0, gene);
-    },
-    selectedUpdateHandler: function() {
-      /* update gene display */
-      let htmlString = this.selected.map(x => `<span>${x.gene}</span>`).join("");
-      $(data.controlContainer.getElementsByClassName("geneDisplay")[0])
-        .html(htmlString);
-      /* update save btn */
-      $(data.controlContainer.getElementsByClassName("saveSelectButton")[0])
-        .html(`Save (${this.selected.length})`);
-      /* update clear btn */
-      $(data.controlContainer.getElementsByClassName("clearSubset")[0])
-        .html(`Clear (${this.selected.length})`);
-    },
-    expressionUpdateHandler: function(selectionOccurred, gene) {
-      if (!data.expressionView) return;
-      if (selectionOccurred) {
-        let countsRow = data.countsMatrix[gene.index];
-        updateExpressionPlot(countsRow, data, gene.gene);
-      }
-      else if (this.selected.length > 0) {
-        let last = this.selected[this.selected.length-1];
-        let countsRow = data.countsMatrix[last.index];
-        updateExpressionPlot(countsRow, data, last.gene);
-      }
-      else {
-        clearExpressionPlot(data);
-      }
+
+class State {
+
+  /**
+   * Returns state machine object retaining the current set of selected genes and managing
+   * whether the app is in graph selection mode or table selection mode
+   * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+   * @return {State} state machine object
+   */
+  constructor(data) {
+    this.data = data;
+    this.graphMode = false;
+    this._selected = [];
+  }
+  
+  /**
+   * Returns current selection of genes
+   * @return {Array} Array of currently selected genes
+   */
+  get selected() {
+    return this._selected;
+  }
+
+  /**
+   * Sets a new array of selected genes and re-renders elements accordingly
+   * @param  {Array} selected Array of genes which are currently selected
+   */
+  set selected(selected) {
+    this._selected = selected;
+    let htmlString = selected.map(x => `<span>${x.gene}</span>`).join("");
+    $(this.data.controlContainer.getElementsByClassName("geneDisplay")[0])
+      .html(htmlString);
+    /* update save btn */
+    $(this.data.controlContainer.getElementsByClassName("saveSelectButton")[0])
+      .html(`Save (${selected.length})`);
+    /* update clear btn */
+    $(this.data.controlContainer.getElementsByClassName("clearSubset")[0])
+      .html(`Clear (${selected.length})`);
+  }
+  
+  /**
+   * Adds a gene to the selection if it's not already selected, or remove it otherwise
+   * @param  {Gene} gene Gene data object which has been clicked on
+   */
+  toggleGene(gene) {
+    let loc = containsGene(this.selected, gene);
+    this.selected = loc >= 0 ? remove(this.selected, loc) : this.selected.concat(gene);
+    this._expressionUpdateHandler(loc < 0, gene);
+  }
+  
+  /**
+   * Manages updates to the expression plot based on the most recently selected gene
+   * @param {Boolean} selectionOccurred True if a gene was selected, false if it was de-selected
+   * @param  {Gene} gene Gene data object which has been clicked on
+   */
+  _expressionUpdateHandler(selectionOccurred, gene) {
+    if (!this.data.expressionView) return;
+    if (selectionOccurred) {
+      let countsRow = this.data.countsMatrix[gene.index];
+      updateExpressionPlot(countsRow, this.data, gene.gene);
     }
-  };
-  return state;
+    else if (this.selected.length > 0) {
+      let last = this.selected[this.selected.length-1];
+      let countsRow = this.data.countsMatrix[last.index];
+      updateExpressionPlot(countsRow, this.data, last.gene);
+    }
+    else {
+      clearExpressionPlot(this.data);
+    }
+  }
+
 }
 
+/**
+ * Generates datatable DOM object, state machine and assigns event listeners
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ */
 function setupXYInteraction(data)
 {
 
-  var state = getStateMachine(data);
+  var state = new State(data);
   var datatableEl = document.createElement("TABLE");
   datatableEl.setAttribute("class", "dataTable");
   data.controlContainer.appendChild(datatableEl);
@@ -185,16 +215,24 @@ function setupXYInteraction(data)
   });
 }
 
-
+/**
+ * Shows Save Data options
+ */
 function showDataDropdown() {
   let dataDropdown = document.getElementsByClassName("dataDropdown")[0];
   dropdownOnClick(dataDropdown);
 }
 
+/**
+ * Responds to a click on the Clear datatable button
+ * @param  {Datatable} datatable datatable object
+ * @param  {State} state state machine object returned by getStateMachine()
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ */
 function clearTableListener(datatable, state, data)
 {
   state.graphMode = false;
-  state.setSelected([]);
+  state.selected = [];
   datatable.rows('.selected').nodes().to$().removeClass('selected');
   datatable.search('').columns().search('').draw();       
   data.xyView.data("selected_points", state.selected);
@@ -203,13 +241,16 @@ function clearTableListener(datatable, state, data)
   console.log(state);
 }
 
-
+/**
+ * Listens and responds to click events on the datatable
+ * @param  {Datatable} datatable datatable object
+ * @param  {State} state state machine object returned by getStateMachine()
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ * @param  {Row} row row object in the table clicked on by the user
+ */
 function tableClickListener(datatable, state, data, row)
 {
-  if (state.graphMode)
-  {
-    return;
-  } 
+  if (state.graphMode) return;
   row.toggleClass('selected');
   let datum = datatable.row(row).data();
   state.toggleGene(datum);
@@ -217,6 +258,13 @@ function tableClickListener(datatable, state, data, row)
   data.xyView.runAsync();
 }
 
+/**
+ * Listens and responds to click events on the XY plot
+ * @param  {Datatable} datatable datatable object
+ * @param  {State} state state machine object returned by getStateMachine()
+ * @param  {Datum} datum point on the graph clicked on by the user
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ */
 function XYSignalListener(datatable, state, datum, data)
 {
   if (datum == null) return;
@@ -224,13 +272,15 @@ function XYSignalListener(datatable, state, datum, data)
   {
     state.graphMode = true;
     datatable.rows('.selected').nodes().to$().removeClass('selected');
-    state.setSelected([]);
+    state.selected = [];
   }
 
   state.toggleGene(datum);
+
   // edge case: deselecting last point
   if (state.selected.length == 0)
     state.graphMode = false;
+
   data.xyView.data("selected_points", state.selected);
   data.xyView.runAsync();
 
@@ -239,12 +289,16 @@ function XYSignalListener(datatable, state, datum, data)
   datatable.columns(0).search(regex_search, regex=true, smart=false).draw();
 }
 
+/**
+ * Resets expression plot to a blank slate
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ */
 function clearExpressionPlot(data)
 {
+  
   if (!data.expressionView)
-  {
     return;
-  }
+  
   data.expressionView.data("table", []);
   data.expressionView.signal("title_signal", "");
   data.expressionView.signal("max_count", 0);
@@ -252,8 +306,13 @@ function clearExpressionPlot(data)
   updateAxisMessage(data);
 }
 
-
-function updateExpressionPlot(countsRow, data, gene)
+/**
+ * Updates expression plot for the given gene and sample counts
+ * @param  {CountsRow} countsRow Data object containing sample counts for a given gene
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ * @param  {String} geneName name of gene being displayed
+ */
+function updateExpressionPlot(countsRow, data, geneName)
 {
   let groups = data.groups.group;
   let samples = data.groups.sample;
@@ -273,16 +332,50 @@ function updateExpressionPlot(countsRow, data, gene)
     result.sort((a, b) => levels.indexOf(a.group) - levels.indexOf(b.group));
   }
   data.expressionView.data("table", result);
-  data.expressionView.signal("title_signal", "Gene " + gene.toString());
+  data.expressionView.signal("title_signal", "Gene " + geneName.toString());
   data.expressionView.signal("max_count", Math.max(...result.map(x => x.count)));
   data.expressionView.runAsync();
   updateAxisMessage(data);
 }
 
 /**
+ * Adds y-axis scaling message DOM objects to the expression plot
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ */
+function addAxisMessage(data)
+{
+  var bindings = data.expressionContainer.getElementsByClassName("vega-bindings")[0];
+  var alertBox = document.createElement("div");
+  alertBox.setAttribute("class", "alertBox invisible");
+  data.expressionView.addSignalListener('max_y_axis', 
+    function(name, value) { updateAxisMessage(data) });
+  bindings.appendChild(alertBox);
+}
+
+/**
+ * Updaes the y-axis scaling for the expression plot
+ * @param  {Data} data encapsulated data object containing references to Vega graphs and DOM elements
+ */
+function updateAxisMessage(data)
+{
+  var alertBox = data.expressionContainer.getElementsByClassName("alertBox")[0];
+  let maxCount = data.expressionView.signal("max_count");
+  let userValue = data.expressionView.signal("max_y_axis");
+  if (userValue == null || userValue == "" || Number(userValue) >= maxCount)
+  {
+    alertBox.setAttribute("class", "alertBox invisible");
+  }
+  else
+  {
+    alertBox.innerHTML = `Max count value is ${maxCount}`;
+    alertBox.setAttribute("class", "alertBox danger");
+  }
+}
+
+/**
  * Searches an array gene data objects to determine if it contains a given gene.
  * @param  {Array} arr array of gene data objects.
- * @param  {datum} datum given gene object
+ * @param  {Datum} datum given gene object
  * @return {Integer} -1 if the given gene is not found; index of the gene in arr otherwise.
  */
 function containsGene(arr, datum)
@@ -310,31 +403,4 @@ function remove(arr, i)
 {
   let new_arr = arr.slice(0, i).concat(arr.slice(i+1))
   return new_arr;
-}
-
-function addAxisMessage(data)
-{
-  var bindings = data.expressionContainer.getElementsByClassName("vega-bindings")[0];
-  var alertBox = document.createElement("div");
-  alertBox.setAttribute("class", "alertBox invisible");
-  data.expressionView.addSignalListener('max_y_axis', 
-    function(name, value) { updateAxisMessage(data) });
-  bindings.appendChild(alertBox);
-}
-
-
-function updateAxisMessage(data)
-{
-  var alertBox = data.expressionContainer.getElementsByClassName("alertBox")[0];
-  let maxCount = data.expressionView.signal("max_count");
-  let userValue = data.expressionView.signal("max_y_axis");
-  if (userValue == null || userValue == "" || Number(userValue) >= maxCount)
-  {
-    alertBox.setAttribute("class", "alertBox invisible");
-  }
-  else
-  {
-    alertBox.innerHTML = `Max count value is ${maxCount}`;
-    alertBox.setAttribute("class", "alertBox danger");
-  }
 }
