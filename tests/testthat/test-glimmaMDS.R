868ede648a8013fe3a6f6d83c391e0a294e5230f
hasaru-k <hasaru-k@users.noreply.github.com>
2021-01-07 13:05:14 +1100

    MDS tests

--- tests/testthat/test-glimmaMDS.R
+++ tests/testthat/test-glimmaMDS.R
@@ -44,3 +44,21 @@ test_that("Saving MDS works",
         unlink(testname)
     }
 })
+
+test_that("If groups is a vector, length of vector must be the
+          same as ncol(dge)",
+{
+    for (x in list(dge, dds))
+    {
+        expect_error(glimmaMDS(dge, groups=1:5))
+    }
+})
+
+test_that("If groups is a dataframe, its row no. must be the
+          same as ncol(dge)",
+{
+    for (x in list(dge, dds))
+    {
+        expect_error(glimmaMDS(dge, groups=dge$samples[1:5,]))
+    }
+})
